875e715b1e7f826591ec3934508032b50d11954b
oleganza <oleganza@gmail.com>
2008-06-16 17:03:46 +0400

    first working version

---
+++ examples/hello_world.rb
@@ -0,0 +1,36 @@
+require File.dirname(__FILE__) + '/../lib/literate'
+
+module HelloWorld
+  spec "Box says 'Hello, world!' by default" do 
+    Box.new.say.should == "Hello, world!"
+  end
+  
+  spec "Box says custom message when instantiated with an argument" do 
+    Box.new("Hi!").say.should == "Hi!"
+  end
+  
+  class Box
+    def initialize(msg = "Hello, world!")
+      @msg = msg
+    end
+    def say
+      @msg.freeze
+    end
+  end
+  
+  module Multiplier
+    spec "helper should return x*x" do
+      helper(0).should == 0
+      helper(1).should == 1
+      helper(2).should == 4
+      helper(2**32).should == 2**64
+    end
+    
+    def helper(x)
+      x * x
+    end
+  end
+end
+
+
+p Literate.run
---
+++ lib/literate.rb
@@ -0,0 +1,109 @@
+module Literate
+  def self.run
+    Runner.run
+  end
+  class Runner
+    # Thread-global spec declaration
+    def self.spec(*args, &block)
+      runner = (Thread.current["literate_spec_runner"] ||= new)
+      runner.spec(*args, &block)
+    end
+    
+    def self.run
+      if r = Thread.current["literate_spec_runner"]
+        r.run
+      else
+        []
+      end
+    end
+    
+    def initialize
+      @specs = []
+    end
+    
+    # Per-instance spec declaration
+    def spec(*args, &block)
+      @specs << Spec.new(*args, &block)
+    end
+    
+    def run
+      @specs.map do |spec|
+        spec.run
+      end
+    end
+  end
+  
+  class Spec
+    attr_accessor :context, :description, :stack, :block
+    
+    def initialize(ctx, desc, stack, &block)
+      @context     = ctx
+      @description = desc
+      @stack       = stack
+      @block       = block
+    end
+    
+    def run
+      begin
+        # It is, actually, a dirty hack.
+        if Module === @context && !(Class === @context)
+          ctx = @context
+          Class.new{ include ctx }.new.instance_eval(&block)
+        else
+          @context.module_eval(&block)
+        end
+      rescue MatchingError => e
+        "#{@description}: failure (#{e.message})"
+      rescue => e
+        "#{@description}: error (#{e})"
+      else
+        "#{@description}: success."
+      end
+    end
+  end
+  
+  module Matchers
+    class Base
+      def initialize(obj)
+        @obj = obj
+      end
+    end
+    class Should < Base
+      def ==(other)
+        if @obj == other
+          true
+        else
+          raise MatchingError.new, "Expected #{other}, got #{@obj}."
+        end
+      end
+    end
+    class ShouldNot < Base
+      def ==(other)
+        unless @obj == other
+          true
+        else
+          raise MatchingError.new, "Expected not #{other}, got #{@obj}."
+        end
+      end
+    end
+  end
+  
+  class MatchingError < StandardError
+  end
+end
+
+module Kernel
+  def spec(desc = nil, &block)
+    Literate::Runner.spec(self, desc, caller, &block)
+  end
+end
+
+class Object
+  def should
+    Literate::Matchers::Should.new(self)
+  end
+  def should_not
+    Literate::Matchers::ShouldNot.new(self)
+  end
+end
+
